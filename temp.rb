7b45dc96a3a63b3da1d4b5e45001aa9410f58ad2
Michael Newman <coloradohokie@gmail.com>
2020-06-03 18:28:46 +0000

    Automatically backed up by Learn

---
+++ temp.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+person_array = [{name: "Tashawn", interests: ["sports", "tv"]}, {name: "Laura", interests: ["skiing", "books"]}, {name: "Evans", interests: ["sports", "camping"]}]
+
+def say_hi(array)
+  array.map do |person|
+   "Hello #{person["name"]}!"
+  end  
+end
